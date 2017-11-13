21645f0a833cd756a1d60fa9500a3619ef27ac11
Mac User <alexarobinson19@gmail.com>
2017-11-12 23:15:13 -0500

    added comments to index.html and fixed wireframe. Also started adding the beginning of the javascript file and some notes to CSS about styles already added.

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+$("#jack").on("click", function(){
+    selectCharacter();
+    clearBeginning();
+});
+
+$("#sam").on("click", function(){
+    selectCharacter();
+    clearBeginning();
+});
+
+$("#tealc").on("click", function(){
+    selectCharacter();
+    clearBeginning();
+});
+
+$("#daniel").on("click", function(){
+    selectCharacter();
+    clearBeginning();
+});
+
+function selectCharacter(){
+
+}
+
+function clearBeginning(){
+    $("#stat-card").empty();
+    // do animation to get these off screen to the left
+}
